5d6a2becdefe02b6f61105ca704a4c6be70f8f6e
Rostom Halaimia <rostom.halaimia@gmail.com>
2020-05-12 19:21:14 +0200

    changed password validation in  user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     has_many :events, through: :attendances
   
     validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir une adresse email au format valide" }, presence: true, length: { maximum: 256 }
-    validates :encrypted_password, presence: true,length: { maximum: 50 }
+    validates :password, presence: true,length: { maximum: 50 }
     validates :description, presence: true, length: {in: 10..500}
     validates :first_name, presence: true, length: {in: 2..30}            
     validates :last_name, presence: true, length: {in: 2..30}
